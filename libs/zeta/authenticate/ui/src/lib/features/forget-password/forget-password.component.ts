c58f5d069f34cc3ea7c2c3e9fa76de92fb01dfde
kaushal2469 <kaushal2469@gmail.com>
2024-07-12 23:43:38 +0530

    create authenticate forget-password component

---
+++ libs/zeta/authenticate/ui/src/lib/features/forget-password/forget-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-forget-password',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.scss'
+})
+export class ForgetPasswordComponent {}
