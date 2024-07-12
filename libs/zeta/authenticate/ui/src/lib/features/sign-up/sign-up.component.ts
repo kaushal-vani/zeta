b81d1fcfea6f26c35e109f37f8e2cd8137b87b7f
kaushal2469 <kaushal2469@gmail.com>
2024-07-12 23:39:23 +0530

    create authenticate sign-up component

---
+++ libs/zeta/authenticate/ui/src/lib/features/sign-up/sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-sign-up',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {}
