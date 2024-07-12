a06de36bc9d293912d8e2133ee12bf67111a52c8
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 00:01:57 +0530

    create authenticate edit-email component

---
+++ libs/zeta/authenticate/ui/src/lib/features/edit-email/edit-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-edit-email',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edit-email.component.html',
+  styleUrl: './edit-email.component.scss'
+})
+export class EditEmailComponent {}
