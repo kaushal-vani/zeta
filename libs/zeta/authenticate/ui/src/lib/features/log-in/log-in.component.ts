71438f5177779e6a8259638226aa7a8a45fbdb32
kaushal2469 <kaushal2469@gmail.com>
2024-07-12 23:39:04 +0530

    create authenticate log-in component

---
+++ libs/zeta/authenticate/ui/src/lib/features/log-in/log-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-log-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './log-in.component.html',
+  styleUrl: './log-in.component.scss'
+})
+export class LogInComponent {}
