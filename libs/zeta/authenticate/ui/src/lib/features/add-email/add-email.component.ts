905ce749d276259cba75fba8013a79238323a2af
kaushal2469 <kaushal2469@gmail.com>
2024-07-12 23:42:09 +0530

    create authenticate add-email component

---
+++ libs/zeta/authenticate/ui/src/lib/features/add-email/add-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-add-email',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-email.component.html',
+  styleUrl: './add-email.component.scss'
+})
+export class AddEmailComponent {}
