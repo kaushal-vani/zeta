a2e04037dc5fa3525efdfb4d468afa857b124884
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 14:40:02 +0530

    import feature components into authenticate page component

--- libs/zeta/authenticate/ui/src/lib/pages/authenticate-page/authenticate-page.component.ts
+++ libs/zeta/authenticate/ui/src/lib/pages/authenticate-page/authenticate-page.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+/* Feature Components */
+import { AddEmailComponent } from '../../features/add-email/add-email.component';
+import { AddPhoneNumberComponent } from '../../features/add-phone-number/add-phone-number.component';
+import { AuthenticationDrawerComponent } from '../../features/authentication-drawer/authentication-drawer.component';
+import { EditEmailComponent } from '../../features/edit-email/edit-email.component';
+import { EditPhoneNumberComponent } from '../../features/edit-phone-number/edit-phone-number.component';
+import { ForgetPasswordComponent } from '../../features/forget-password/forget-password.component';
+import { LogInComponent } from '../../features/log-in/log-in.component';
+import { OneTimePasswordComponent } from '../../features/one-time-password/one-time-password.component';
+import { SignUpComponent } from '../../features/sign-up/sign-up.component';
+import { VerificationFailureComponent } from '../../features/verification-failure/verification-failure.component';
+import { VerificationSuccessfulComponent } from '../../features/verification-successful/verification-successful.component';
 
 @Component({
   selector: 'lib-authenticate-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AddEmailComponent,
+    AddPhoneNumberComponent,
+    AuthenticationDrawerComponent,
+    EditEmailComponent,
+    EditPhoneNumberComponent,
+    ForgetPasswordComponent,
+    LogInComponent,
+    OneTimePasswordComponent,
+    SignUpComponent,
+    VerificationFailureComponent,
+    VerificationSuccessfulComponent
+  ],
   templateUrl: './authenticate-page.component.html',
   styleUrl: './authenticate-page.component.scss'
 })
-export class AuthenticatePageComponent {}
+export class AuthenticatePageComponent {
+  currentView = '';
+}
