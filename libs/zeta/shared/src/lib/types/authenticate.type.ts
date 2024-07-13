cbbcc6d2a40c2e02cc3fc91592de0544b937ebc9
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 16:36:15 +0530

    create ScreenView and ButtonName in types

---
+++ libs/zeta/shared/src/lib/types/authenticate.type.ts
@@ -0,0 +1,28 @@
+export type ScreenView =
+  | 'add-email'
+  | 'add-phone-number'
+  | 'authentication-drawer'
+  | 'edit-email'
+  | 'edit-phone-number'
+  | 'forgot-password'
+  | 'login'
+  | 'one-time-password'
+  | 'sign-up'
+  | 'verification-failure'
+  | 'verification-successful';
+
+export type ButtonName =
+  | 'add'
+  | 'cancel'
+  | 'continue'
+  | 'edit'
+  | 'forgot-password'
+  | 'go-to-login'
+  | 'go-to-sign-up'
+  | 'login'
+  | 'logout'
+  | 'otp-submit'
+  | 'resend'
+  | 'set-up-later'
+  | 'sign-up'
+  | 'verify';
