221d306ba881d0dc49fa29cb6531b2d8322f4365
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 08:23:12 +0530

    update authenticate features index ts

---
+++ libs/zeta/authenticate/ui/src/index.ts
@@ -0,0 +1,13 @@
+/* Features */
+
+export * from './lib/features/add-email/add-email.component';
+export * from './lib/features/add-phone-number/add-phone-number.component';
+export * from './lib/features/authentication-drawer/authentication-drawer.component';
+export * from './lib/features/edit-email/edit-email.component';
+export * from './lib/features/edit-phone-number/edit-phone-number.component';
+export * from './lib/features/forget-password/forget-password.component';
+export * from './lib/features/log-in/log-in.component';
+export * from './lib/features/one-time-password/one-time-password.component';
+export * from './lib/features/sign-up/sign-up.component';
+export * from './lib/features/verification-failure/verification-failure.component';
+export * from './lib/features/verification-successful/verification-successful.component';
