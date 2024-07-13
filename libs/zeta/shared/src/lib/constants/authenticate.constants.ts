d20c72db0d767005c10983dc1191eba852ed6a2b
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 17:12:23 +0530

    add VIEW_NAME and DEVICE_TYPE in constant ts

---
+++ libs/zeta/shared/src/lib/constants/authenticate.constants.ts
@@ -0,0 +1,18 @@
+export const VIEW_NAME = {
+  ADD_EMAIL: 'addEmail',
+  ADD_PHONE_NUMBER: 'addPhoneNumber',
+  AUTHENTICATION_DRAWER: 'authenticationDrawer',
+  EDIT_EMAIL: 'editEmail',
+  EDIT_PHONE_NUMBER: 'editPhoneNumber',
+  FORGOT_PASSWORD: 'forgotPassword',
+  LOGIN: 'login',
+  ONE_TIME_PASSWORD: 'oneTimePassword',
+  SIGN_UP: 'signUp',
+  VERIFICATION_FAILURE: 'verificationFailure',
+  VERIFICATION_SUCCESSFUL: 'verificationSuccessful'
+};
+
+export const DEVICE_NAME = {
+  PHONE_NUMBER: 'phoneNumber',
+  EMAIL: 'email'
+};
