3e180f0d4fc1f7e0290904446f0e7e1b4dfecaa2
kaushal2469 <kaushal2469@gmail.com>
2024-07-13 14:48:02 +0530

    add SCREEN_VIEW and BUTTON_NAME in authenticate enum

---
+++ libs/zeta/shared/src/lib/enums/authenticate.enum.ts
@@ -0,0 +1,30 @@
+export const enum SCREEN_VIEW {
+  ADD_EMAIL = 'add-email',
+  ADD_PHONE_NUMBER = 'add-phone-number',
+  AUTHENTICATION_DRAWER = 'authentication-drawer',
+  EDIT_EMAIL = 'edit-email',
+  EDIT_PHONE_NUMBER = 'edit-phone-number',
+  FORGOT_PASSWORD = 'forgot-password',
+  LOGIN = 'login',
+  ONE_TIME_PASSWORD = 'one-time-password',
+  SIGN_UP = 'sign-up',
+  VERIFICATION_FAILURE = 'verification-failure',
+  VERIFICATION_SUCCESSFUL = 'verification-successful'
+}
+
+export const enum BUTTON_NAME {
+    CANCEL = 'cancel',
+    CONTINUE = 'continue',
+    EDIT = 'edit',
+    ADD = 'add',
+    FORGOT_PASSWORD = 'forgot-password',
+    GO_TO_LOGIN = 'go-to-login',
+    GO_TO_SIGN_UP = 'go-to-sign-up',
+    LOGIN = 'login',
+    LOGOUT = 'logout',
+    OTP_SUBMIT = 'otp-submit',
+    RESEND = 'resend',
+    SIGN_UP = 'sign-up',
+    VERIFY = 'verify',
+    SET_UP_LATER = 'set-up-later'
+}
