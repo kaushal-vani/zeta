ec04e3eba8f226e0bbc1d08d8cf54dbd00f67f8f
kaushal2469 <kaushal2469@gmail.com>
2024-07-14 14:35:58 +0530

    create spin loader shared component

--- libs/zeta/shared/src/lib/components/loader/loader.component.stories.ts
+++ libs/zeta/shared/src/lib/components/loader/loader.component.stories.ts
@@ -1,24 +1,30 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { LoaderComponent } from './loader.component';
 
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta: Meta<LoaderComponent> = {
   component: LoaderComponent,
-  title: 'LoaderComponent'
+  title: 'LoaderComponent',
+  argTypes: {
+    isLoading: { control: 'boolean' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large']
+    }
+  },
 };
 export default meta;
 type Story = StoryObj<LoaderComponent>;
 
-export const Primary: Story = {
-  args: {}
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+  },
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/loader works!/gi)).toBeTruthy();
-  }
+export const Loaded: Story = {
+  args: {
+    isLoading: false,
+  },
 };
+
--- libs/zeta/shared/src/lib/components/loader/loader.component.ts
+++ libs/zeta/shared/src/lib/components/loader/loader.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Size } from '../../types/authenticate.type';
 
 @Component({
   selector: 'lib-loader',
@@ -8,4 +9,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
-export class LoaderComponent {}
+export class LoaderComponent {
+  @Input() isLoading = true;
+  @Input() size: Size = 'medium';
+
+  get sizeClass(): string {
+    return this.size;
+  }
+}
