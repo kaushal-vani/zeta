9334efce84ff52fc3f8029d43121a9c904f25b0b
kaushal2469 <kaushal2469@gmail.com>
2024-07-14 17:27:12 +0530

    update shared index ts with constants

--- libs/zeta/shared/src/index.ts
+++ libs/zeta/shared/src/index.ts
@@ -3,6 +3,9 @@ export * from './lib/components/footer/footer.component';
 export * from './lib/components/header/header.component';
 export * from './lib/components/loader/loader.component';
 
+/* Constant */
+export * from './lib/constants/authenticate.constants';
+
 /* Enum */
 export * from './lib/enums/authenticate.enum';
 
