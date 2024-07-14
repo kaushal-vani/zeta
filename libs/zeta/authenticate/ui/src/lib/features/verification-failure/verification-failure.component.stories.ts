34b9aed2f9801571bc7f5a0cc722a563971244f8
kaushal2469 <kaushal2469@gmail.com>
2024-07-14 16:33:55 +0530

    verification failure svg and text

--- libs/zeta/authenticate/ui/src/lib/features/verification-failure/verification-failure.component.stories.ts
+++ libs/zeta/authenticate/ui/src/lib/features/verification-failure/verification-failure.component.stories.ts
@@ -1,8 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { VerificationFailureComponent } from './verification-failure.component';
 
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta: Meta<VerificationFailureComponent> = {
   component: VerificationFailureComponent,
@@ -11,14 +9,9 @@ const meta: Meta<VerificationFailureComponent> = {
 export default meta;
 type Story = StoryObj<VerificationFailureComponent>;
 
-export const Primary: Story = {
+
+export const VerificationFailed: Story = {
   args: {}
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/verification-failure works!/gi)).toBeTruthy();
-  }
-};
+
