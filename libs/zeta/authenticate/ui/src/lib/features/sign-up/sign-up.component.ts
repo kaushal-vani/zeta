999a4a8b418b74291dd285958e9eb4d17c3059de
kaushal2469 <kaushal2469@gmail.com>
2024-07-15 22:26:40 +0530

    sign up screen navigate flow

--- libs/zeta/authenticate/ui/src/lib/features/sign-up/sign-up.component.ts
+++ libs/zeta/authenticate/ui/src/lib/features/sign-up/sign-up.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from 'zenova/components';
+import { SCREEN_VIEW, ScreenAction } from '@zeta/zeta/shared';
 
 @Component({
   selector: 'lib-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+
+  createAccount() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.SIGN_UP
+    });
+  }
+}
