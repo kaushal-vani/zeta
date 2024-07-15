2ffcf6da60ee8915936de4b1c9fa3578e9cee962
kaushal2469 <kaushal2469@gmail.com>
2024-07-15 22:25:44 +0530

    authenticate drawer screen navigate flow

--- libs/zeta/authenticate/ui/src/lib/features/authentication-drawer/authentication-drawer.component.ts
+++ libs/zeta/authenticate/ui/src/lib/features/authentication-drawer/authentication-drawer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DEVICE, SCREEN_VIEW, ScreenAction } from '@zeta/zeta/shared';
 
 @Component({
   selector: 'lib-authentication-drawer',
@@ -8,4 +9,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './authentication-drawer.component.html',
   styleUrl: './authentication-drawer.component.scss'
 })
-export class AuthenticationDrawerComponent {}
+export class AuthenticationDrawerComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+
+  addPhoneNumber() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.AUTHENTICATION_DRAWER,
+      nextView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+      deviceType: DEVICE.Phone_NUMBER,
+    });
+  }
+
+  addEmailAddress() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.AUTHENTICATION_DRAWER,
+      nextView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+      deviceType: DEVICE.EMAIL
+    });
+  }
+}
