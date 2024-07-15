b9d3481ddff3217a840e626d2efcc21453ede334
kaushal2469 <kaushal2469@gmail.com>
2024-07-15 22:24:14 +0530

    create screen navigation service

--- libs/zeta/shared/src/lib/services/screen-navigation.service.ts
+++ libs/zeta/shared/src/lib/services/screen-navigation.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ScreenAction } from '../models/authenticate.interfaces';
+import { SCREEN_VIEW } from '../enums/authenticate.enum';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScreenNavigationService {
+  private route = inject(Router)
+  currentView = SCREEN_VIEW.NONE || SCREEN_VIEW.SIGN_UP
 
-  constructor() { }
+  initiateRedirect(screen: ScreenAction): void {
+    if(screen.currentView === SCREEN_VIEW.SIGN_UP){
+      this.route.navigate(['/home'])
+    };
+    if(screen.currentView === SCREEN_VIEW.AUTHENTICATION_DRAWER){
+      this.currentView = screen.nextView as SCREEN_VIEW;
+    }
+  }
 }
