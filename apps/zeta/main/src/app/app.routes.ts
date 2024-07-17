25c4195ffc19c666892a21a1cf557dc8895f7411
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 23:02:58 +0530

    update app routes ts

--- apps/zeta/main/src/app/app.routes.ts
+++ apps/zeta/main/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from '@zeta/zeta/home/ui';
 import { ProfilePageComponent } from '@zeta/zeta/profile/ui';
 import { StatsPageComponent } from '@zeta/zeta/stats/ui';
 import { TaskPageComponent } from '@zeta/zeta/task/ui';
+import { TimelinePageComponent } from '@zeta/zeta/timeline/ui';
 
 export const appRoutes: Route[] = [
   { path: 'authenticate', component: AuthenticatePageComponent },
@@ -11,6 +12,7 @@ export const appRoutes: Route[] = [
   { path: 'task', component: TaskPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'stats', component: StatsPageComponent },
+  { path: 'timeline', component: TimelinePageComponent },
   { path: '', redirectTo: '/authenticate', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
