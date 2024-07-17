565a9a4df84a5d6c81e6b42c30ad6cd0c4c95d34
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 23:01:33 +0530

    create timeline page component

---
+++ libs/zeta/timeline/ui/src/lib/pages/timeline-page/timeline-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-timeline-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './timeline-page.component.html',
+  styleUrl: './timeline-page.component.scss'
+})
+export class TimelinePageComponent {}
