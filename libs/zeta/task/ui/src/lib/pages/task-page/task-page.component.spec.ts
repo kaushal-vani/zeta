d42b87bd935470de26985deada739b65ce348ee3
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 22:59:14 +0530

    create task page component

---
+++ libs/zeta/task/ui/src/lib/pages/task-page/task-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TaskPageComponent } from './task-page.component';
+
+describe('TaskPageComponent', () => {
+  let component: TaskPageComponent;
+  let fixture: ComponentFixture<TaskPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskPageComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TaskPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
