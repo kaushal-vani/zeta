e8a9e4291e715929c400d2c4dd4f130268dc42b4
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 22:59:50 +0530

    create pages routes , wild card route and empty route

--- apps/zeta/main/src/app/app.routes.ts
+++ apps/zeta/main/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { AuthenticatePageComponent } from '@zeta/zeta/authenticate/ui';
+import { HomePageComponent } from '@zeta/zeta/home/ui';
+import { ProfilePageComponent } from '@zeta/zeta/profile/ui';
+import { StatsPageComponent } from '@zeta/zeta/stats/ui';
+import { TaskPageComponent } from '@zeta/zeta/task/ui';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'authenticate', component: AuthenticatePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'task', component: TaskPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'stats', component: StatsPageComponent },
+  { path: '', redirectTo: '/authenticate', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
