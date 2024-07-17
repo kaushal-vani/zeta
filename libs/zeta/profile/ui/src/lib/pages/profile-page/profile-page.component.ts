e573dd80e5b90443a4d42aece28d7f91671ca1e5
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 22:54:38 +0530

    create profile page component

---
+++ libs/zeta/profile/ui/src/lib/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-profile-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss'
+})
+export class ProfilePageComponent {}
