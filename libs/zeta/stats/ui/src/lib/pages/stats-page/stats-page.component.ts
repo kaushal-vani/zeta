ed3b649cbc1c142d019b608d1c0e17c8239ded0b
kaushal2469 <kaushal2469@gmail.com>
2024-07-17 22:58:58 +0530

    create stats page component

---
+++ libs/zeta/stats/ui/src/lib/pages/stats-page/stats-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-stats-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stats-page.component.html',
+  styleUrl: './stats-page.component.scss'
+})
+export class StatsPageComponent {}
